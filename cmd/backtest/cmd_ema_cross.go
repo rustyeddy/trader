d38fb9c1369ad847c27e5702189dbda1376a9a93
Rusty Eddy <rusty@rustyeddy.com>
2026-02-17 21:02:43 -0800

    moved and removed some stuff

--- cmd/backtest/cmd_ema_cross.go
+++ cmd/backtest/cmd_ema_cross.go
@@ -1,13 +1,12 @@
 package backtest
 
 import (
-	"fmt"
-
 	"github.com/rustyeddy/trader/market/strategies"
 	"github.com/spf13/cobra"
 )
 
 var cfg = strategies.EMACrossConfig{}
+
 var CMDBacktestEMACross = &cobra.Command{
 	Use:   "ema-cross",
 	Short: "Run EMA Cross backtest strategy on H1",
@@ -15,19 +14,22 @@ var CMDBacktestEMACross = &cobra.Command{
 }
 
 func init() {
+
+	scfg := strategies.StrategyConfig{
+		Balance: 1000,
+		Stop:    20,
+		Take:    40,
+		RR:      0.02,
+
+		File: "data/prices/derived/eur_usd/d1/eur_usd-d1-2025.csv",
+	}
+	cfg.StrategyConfig = scfg
+
 	cmd := CMDBacktestEMACross
 	cmd.Flags().StringVar(&cfg.File, "file", "", "Path to Dukascopy-style M1 candles file (semicolon-separated)")
-	// cmd.Flags().StringVar(&instrument, "instrument", "EUR_USD", "Instrument (e.g. EUR_USD)")
-	// cmd.Flags().IntVar(&fast, "fast", 9, "Fast EMA period")
-	// cmd.Flags().IntVar(&slow, "slow", 21, "Slow EMA period")
-	// cmd.Flags().Float64Var(&minSpread, "min-spread", 0, "Min |fast-slow| (price units) required to signal; 0 disables")
-	// cmd.Flags().IntVar(&minValid, "min-valid", 50, "Minimum valid M1 bars per hour to keep an H1 candle")
-
 }
 
 func RunEMACross(cmd *cobra.Command, args []string) error {
-
-	fmt.Println("EMA Cross backtest strategy")
 
 	return nil
 }
