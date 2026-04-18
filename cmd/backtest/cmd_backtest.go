ea1af5ec3f32b4f0ed591b8d37780d3628eef7ef
Rusty Eddy <rusty@rustyeddy.com>
2026-04-18 11:20:37 -0700

    Issue #81: add top-level config-driven backtest runner entrypoint

--- cmd/backtest/cmd_backtest.go
+++ cmd/backtest/cmd_backtest.go
@@ -1,11 +1,16 @@
 package backtest
 
 import (
+	"strings"
+
 	traderpkg "github.com/rustyeddy/trader"
 	"github.com/spf13/cobra"
 )
 
 var rootCfg *traderpkg.RootConfig
+var btReportsDir string
+
+const defaultReportsDir = "reports"
 
 func New(rc *traderpkg.RootConfig) *cobra.Command {
 	rootCfg = rc
@@ -15,9 +20,16 @@ func New(rc *traderpkg.RootConfig) *cobra.Command {
 func init() {
 	CMDBacktest.AddCommand(CMDBacktestEMACross)
 	CMDBacktest.AddCommand(CMDBacktestEMACrossADX)
+	CMDBacktest.Flags().StringVar(&btReportsDir, "reports", defaultReportsDir, "Directory for generated backtest reports")
 }
 
 var CMDBacktest = &cobra.Command{
 	Use:   "backtest",
 	Short: "Run backtests on historical data",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if rootCfg != nil && strings.TrimSpace(rootCfg.ConfigPath) != "" {
+			return runBacktestConfigBatch(cmd)
+		}
+		return cmd.Help()
+	},
 }
