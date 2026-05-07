3f63f82d94be8c7d18883c7d049e52b6579c5267
Rusty Eddy <rusty@rustyeddy.com>
2026-05-07 04:56:26 +0000

    add filter dukascopy

---
+++ dukascopy_test.go
@@ -0,0 +1,16 @@
+package trader
+
+import (
+	"os"
+	"testing"
+)
+
+const dukascopyTestsEnv = "TRADER_RUN_DUKASCOPY_TESTS"
+
+func requireDukascopyTests(t *testing.T) {
+	t.Helper()
+	if os.Getenv(dukascopyTestsEnv) == "1" {
+		return
+	}
+	t.Skip("skipping Dukascopy tests; set TRADER_RUN_DUKASCOPY_TESTS=1 to enable")
+}
